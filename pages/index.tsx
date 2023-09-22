926a8e80722f2c0c15bc269af6e62a127bec0957
Amin <aminmajid.dev@gmail.com>
2023-09-22 23:50:05 +0500

    chat ai

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Nav from "@/sections/Nav";
 import ExplodedViews from "@/sections/ExplodedViews";
 import ClassNames from "@/components/ClassNames";
 import ErrorCodes from "@/sections/ErrorCodes";
+import IntelligentDocs from "@/sections/IntelligentDocs";
 
 const ubuntu = Ubuntu({
   subsets: ["latin"],
@@ -21,6 +22,7 @@ export default function Home() {
         <Parts />
         <ExplodedViews />
         <ErrorCodes />
+        <IntelligentDocs />
       </main>
     </>
   );
--- sections/IntelligentDocs.tsx
+++ sections/IntelligentDocs.tsx
@@ -2,7 +2,30 @@ import { useState, useEffect, useRef } from "react";
 import SectionGrid from "./SectionGrid";
 import ClassNames from "@/components/ClassNames";
 
-export default function ErrorCodes() {
+const messages = [
+  {
+    type: "user",
+    text: "Hi, I'm looking for the manual for my refrigerator",
+  },
+  {
+    type: "bot",
+    text: "Sure, I can help you with that. What is the model number of your refrigerator?",
+  },
+  {
+    type: "user",
+    text: "JF42NXFXDE02",
+  },
+  {
+    type: "bot",
+    text: "Great, I found the manual for your refrigerator. Is there anything else I can help you with?",
+  },
+  {
+    type: "user",
+    text: "Thanks!",
+  },
+];
+
+export default function IntelligentDocs() {
   const [selected, setSelected] = useState<number>();
 
   const search = useRef<HTMLInputElement>(null);
@@ -18,24 +41,85 @@ export default function ErrorCodes() {
 
   return (
     <SectionGrid center row>
-      <div className="relative mb-4">
-        <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-          Error Code Search
-        </h2>
-      </div>
+      <div className="relative mb-4"></div>
       <div className="flex">
         <div className="w-1/2">
-          <div className="relative w-96 mb-5">
-            <input
-              ref={search}
-              type="text"
-              className="w-full px-4 py-2 bg-transparent border-2 border-slate-200 rounded-lg focus:outline-none focus:border-blue-100"
-              placeholder="Enter error code to search"
-            />
-            <i className="fi fi-rr-search text-xl text-gray-300 absolute right-4 top-2" />
+          <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            Explore Docs
+          </h2>
+          <div className="relative mb-5 flex flex-wrap flex-row items-center gap-5">
+            <div className="mt-5 flex items-center gap-5 text-gray-800 transition delay-700 animate-[pulse_0.5s_ease-in-out]">
+              <i className="fi fi-rr-poll-h text-lg p-4 bg-gray-200 rounded-full flex items-center justify-center"></i>
+              <div>
+                <div className="text-sm">Manual-JF42NXFXDE02.PDF</div>
+                <div className="text-xs">Page 25, Line 48</div>
+              </div>
+            </div>
+            <div className="mt-5 flex items-center gap-5 text-gray-800 transition delay-700 animate-[pulse_0.5s_ease-in-out]">
+              <i className="fi fi-rr-poll-h text-lg p-4 bg-gray-200 rounded-full flex items-center justify-center"></i>
+              <div>
+                <div className="text-sm">Manual-JF42NXFXDE02.PDF</div>
+                <div className="text-xs">Page 25, Line 48</div>
+              </div>
+            </div>
+            <div className="mt-5 flex items-center gap-5 text-gray-800 transition delay-700 animate-[pulse_0.5s_ease-in-out]">
+              <i className="fi fi-rr-poll-h text-lg p-4 bg-gray-200 rounded-full flex items-center justify-center"></i>
+              <div>
+                <div className="text-sm">Manual-JF42NXFXDE02.PDF</div>
+                <div className="text-xs">Page 25, Line 48</div>
+              </div>
+            </div>
+            <div className="mt-5 flex items-center gap-5 text-gray-800 transition delay-700 animate-[pulse_0.5s_ease-in-out]">
+              <i className="fi fi-rr-poll-h text-lg p-4 bg-gray-200 rounded-full flex items-center justify-center"></i>
+              <div>
+                <div className="text-sm">Manual-JF42NXFXDE02.PDF</div>
+                <div className="text-xs">Page 25, Line 48</div>
+              </div>
+            </div>
+            <div className="mt-5 flex items-center gap-5 text-gray-800 transition delay-700 animate-[pulse_0.5s_ease-in-out]">
+              <i className="fi fi-rr-poll-h text-lg p-4 bg-gray-200 rounded-full flex items-center justify-center"></i>
+              <div>
+                <div className="text-sm">Manual-JF42NXFXDE02.PDF</div>
+                <div className="text-xs">Page 25, Line 48</div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="flex w-1/2 flex-col gap-4 relative"></div>
+        <div className="w-1/2 flex-col gap-4 relative">
+          <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            Intelligent Docs
+          </h2>
+          <div className="flex flex-col p-5 bg-slate-200 rounded-xl">
+            <div className="flex-1 overflow-y-auto flex flex-col">
+              {messages.map((message, index) => (
+                <div
+                  key={index}
+                  className={ClassNames(
+                    "p-2 max-w-md my-2 rounded-lg text-sm",
+                    message.type === "user"
+                      ? "bg-blue-600 text-white ml-auto"
+                      : "bg-slate-100 mr-auto"
+                  )}
+                >
+                  {message.text}
+                </div>
+              ))}
+            </div>
+            <div className="mt-5">
+              <div className="flex relative">
+                <input
+                  type="text"
+                  className="flex-grow rounded-lg shadow p-3 pb-10 outline-none"
+                  placeholder="Type your message..."
+                />
+                <div className="bg-white absolute right-3 top-3 flex flex-col gap-3">
+                  <i className="fi fi-rr-microphone"></i>
+                  <i className="fi fi-rr-paper-plane-top"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </SectionGrid>
   );
