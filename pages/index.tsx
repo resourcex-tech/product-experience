ee2153758387f4ddd272e354d95c098e1305091a
Amin <aminmajid.dev@gmail.com>
2023-09-27 10:31:26 +0500

    api connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,19 +10,17 @@ const ubuntu = Ubuntu({
   weight: ["300", "400", "500", "700"],
 });
 
-const model = [
-  {
-    id: 1,
-    name: "Leslie Alexander",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  // More users...
-];
+interface Model {
+  model_id: string;
+  model_number: string;
+  manufucture_name: string;
+  category_name: string;
+  model_stock_photo: string;
+}
 
 export default function Home() {
   const [query, setQuery] = useState("");
-  const [selectedModel, setSelectedModel] = useState();
+  const [selectedModel, setSelectedModel] = useState<Model>();
   const [loading, setLoading] = useState(false);
 
   const [filteredModel, setFilteredPeople] = useState([]);
@@ -31,7 +29,7 @@ export default function Home() {
     if (query.length > 2) {
       setLoading(true);
       axios
-        .get(`http://products-api.macaan.ai/v1/search/models/${query}`)
+        .get(`https://products-api.macaan.ai/v1/search/models/${query}`)
         .then((response) => {
           console.log(response.data);
           setFilteredPeople(response.data);
@@ -49,7 +47,7 @@ export default function Home() {
   const router = useRouter();
   useEffect(() => {
     if (selectedModel) {
-      router.push(`/product/${selectedModel["model/id"]}`);
+      router.push(`/product/${selectedModel.model_id}`);
     }
   }, [selectedModel]);
 
@@ -72,7 +70,7 @@ export default function Home() {
               <Combobox.Input
                 className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
                 onChange={(event) => setQuery(event.target.value)}
-                displayValue={(model: any) => model["model/number"]}
+                displayValue={(model: any) => model.model_id}
               />
               <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
                 {loading ? (
@@ -84,14 +82,14 @@ export default function Home() {
 
               {filteredModel.length > 0 && (
                 <Combobox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                  {filteredModel.map((model) => (
+                  {filteredModel.map((model: any, index: number) => (
                     <Combobox.Option
-                      key={model["model/id"]}
+                      key={index}
                       value={model}
                       className={({ active }) =>
                         ClassNames(
-                          "relative cursor-default select-none py-2 pl-3 pr-9",
-                          active ? "bg-indigo-600 text-white" : "text-gray-900"
+                          "group relative cursor-default select-none py-2 pl-3 pr-9",
+                          active ? "bg-blue-600 text-white" : "text-gray-900"
                         )
                       }
                     >
@@ -103,6 +101,16 @@ export default function Home() {
                               alt=""
                               className="h-6 w-6 flex-shrink-0 rounded-full"
                             /> */}
+                            {model.model_stock_photo ? (
+                              <div className="w-10 h-10 bg-white p-1 rounded flex items-center justify-center mr-2">
+                                <img
+                                  src={model.model_stock_photo}
+                                  alt={model.model_number}
+                                />
+                              </div>
+                            ) : (
+                              <div className="w-10 h-10 bg-slate-200 p-1 rounded flex items-center justify-center mr-2"></div>
+                            )}
                             <div>
                               <div
                                 className={ClassNames(
@@ -110,10 +118,31 @@ export default function Home() {
                                   selected && "font-semibold"
                                 )}
                               >
-                                {model["model/number"]}
+                                {model.model_number}
                               </div>
-                              <div className="text-xs text-gray-500 ml-3">
-                                {model["category/name"]}
+                              <div className="flex items-center">
+                                <div
+                                  className={ClassNames(
+                                    "text-xs text-gray-500 ml-3",
+                                    active && "text-blue-100"
+                                  )}
+                                >
+                                  {model.manufucture_name}
+                                </div>
+                                <div
+                                  className={ClassNames(
+                                    "w-1 h-1 rounded-full bg-gray-500 ml-2",
+                                    active && "bg-blue-300"
+                                  )}
+                                ></div>
+                                <div
+                                  className={ClassNames(
+                                    "text-xs text-gray-500 ml-2",
+                                    active && "text-blue-100"
+                                  )}
+                                >
+                                  {model.category_name}
+                                </div>
                               </div>
                             </div>
                           </div>
